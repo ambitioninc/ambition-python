8680aa15fb0fdd47f40da05b4965a48f2304ad2d
thinkt4nk <thinkt4nk@gmail.com>
2015-09-02 16:32:56 -0400

    pep8

--- ambition/rest.py
+++ ambition/rest.py
@@ -5,7 +5,6 @@ Credit: this file (rest.py) is modified based on rest.py in Dropbox Python SDK:
 https://www.dropbox.com/developers/core/sdks/python
 """
 
-import sys
 import io
 import json
 import ssl
